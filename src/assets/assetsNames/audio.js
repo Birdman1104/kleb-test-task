232095887ee675eb6aec78ea419ee84c75420479
Birdman1104 <59249974+Birdman1104@users.noreply.github.com>
2023-09-05 21:25:02 +0000

    Deploying to gh-pages from @ Birdman1104/kleb-test-task@20f2d40990046dbf6240490f600fdb85ba612b2c 🚀

--- src/assets/assetsNames/audio.js
+++ src/assets/assetsNames/audio.js
@@ -1,1 +1,1 @@
-export const audioAssets=[];
+export const audioAssets=[{name:"effect",path:"assets/audio/effect.mp3"},{name:"tap",path:"assets/audio/tap.mp3"},{name:"win",path:"assets/audio/win.wav"}];
